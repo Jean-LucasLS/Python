b4af03a0e32a1c8fd6d74e486ed962c127cd7420
Jean-Lucas <jean.lucasls96@gmail.com>
2022-10-13 14:17:50 -0300

    edit

--- Basic - Python/main.py
+++
@@ -1,6 +0,0 @@
-name = input('Digite algo: ')
-n = name
-print(n.isnumeric()) #verificar se é somente numérico
-print(n.isalnum()) #verificar se é alfanumérico
-print(n.isalpha()) #verificar se há somente letras (espaços geram negação)
-print('O {} e {} foram igualados.'.format(n, name))
---
+++ Basic Python/printf(sep&end).py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+print('Meu nome é', nome, sep=' ', end='')
+print(', minha idade é', idade, end='')
+print(', e meu cpf é', end=' ')
+print('492', '453', '342', sep='.', end='-')
+print('34')
