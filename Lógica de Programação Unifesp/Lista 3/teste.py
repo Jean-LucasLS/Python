565766090a0a91450b1018af6ebd0ee52f525173
Jean-Lucas <jean.lucasls96@gmail.com>
2022-10-19 12:55:09 -0300

    restuct

--- Lógica de Programação Unifesp/Lista 3/teste.py
+++
@@ -1,4 +0,0 @@
-palavra = ['n', 'v', 'e', 'l', 'o', 'p', 'e'] #Jean-Lucas Luquetti Silva / RA: 120443
-invertido = palavra[::-1] #declaração da palavra numa string e comando para invertê-la e definir a inversão na varirável "invertido"
-for i in range(len(palavra)): #laço que lê e imprime letra por letra considerando o tamanho da palavra
-	print(invertido[i])
