aa2d5e63c351dcea3e410e0a30fca8b2ca9e734e
Jean-Lucas <jean.lucas@autaza.com>
2023-01-27 10:25:22 -0300

    Jockey

--- Treinamento/POO/teste.py
+++
@@ -1,8 +0,0 @@
-from personclass import Person # Importa do arquivo "personclass.py" a classe "Class Person".
-p1 = Person('Jonas', 8)
-p2 = Person('James', 1)
-p1.name = 'Jean'
-p2.name = 'Joe'
-p1.age = 26
-p2.age = p1.age
-p1.AgeCor()
