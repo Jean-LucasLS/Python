2f8af20861f05ad4b2e55a9246bdd78630a4d972
Jean-Lucas <jean.lucasls96@gmail.com>
2023-09-18 15:56:30 -0300

    correction

--- Basic Python/file/file_with_open.py
+++ Basic Python/file/file_with_open.py
@@ -3,7 +3,8 @@ FILE_PATH = 'file/file_with_open.txt'
 # file = open(FILE_PATH, 'w')
 # file.close()
 
-with open(FILE_PATH, 'w+', encoding='utf8') as file: # 'w' erase all the currennt file content
+# 'w' erase all the currennt file content IF a command to write in the file is given
+with open(FILE_PATH, 'r', encoding='utf8') as file:
   print(type(file))
   file.write('Doc\nJucaju')
   file.seek(0,0) # Moves the cursos to the (0, 0) point
